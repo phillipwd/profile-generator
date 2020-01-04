7121e42a26f49dd6329df2eb989e6f9d6dbe403b
Phillip Dunham <phillipwwd@gmail.com>
2020-01-03 20:27:51 -0800

    Create and populate object with required info from github

--- Develop/index.js
+++ Develop/index.js
@@ -3,11 +3,13 @@ const fs = require("fs");
 const axios = require("axios");
 
 const questions = [
-    {type: "input", name: "name", message:"What is your Name?"},
+    // {type: "input", name: "name", message:"What is your Name?"},
     {type: "input", name: "github", message:"What is your GitHub username?"},
-    {type: "input", name: "blog", message: "What is your blog address?"}
+    // {type: "input", name: "blog", message: "What is your blog address?"}
 ];
 let answers = [];
+
+let profileRequirements = {};
 
 async function getInfo(){
     try{ 
@@ -17,20 +19,51 @@ async function getInfo(){
         console.log(err);    
     };
     try{
-        const gitUrl = "https://github.com/" + answers.github;
-        axios.get(gitUrl).then(function(reply){
-            console.log(reply);
-            
-            const repoNames = reply.data//.repos;
-        });
+        const gitProfileUrl = "https://api.github.com/users/" + answers.github
+        const profileData = await axios.get(gitProfileUrl)
+        profileRequirements.avatarUrl = profileData.data.avatar_url;
+        profileRequirements.userName = profileData.data.login;
+        profileRequirements.githubProfile = profileData.data.html_url;
+        profileRequirements.blog = profileData.data.blog;
+        profileRequirements.following = profileData.data.following;
+        profileRequirements.followers = profileData.data.followers;
+        profileRequirements.repos = profileData.data.public_repos;
+        profileRequirements.bio = profileData.data.bio;
+console.log(profileRequirements);
+
+        
+
+    }
+    catch(err){
+        console.log(err);
+    }
+
+    try{
+
     }
     catch(err){
         console.log(err);
         
-    };
-    // (console.log(answers.name));getting answers from prompt works
-};
+    }
 
+    try{
+        const reposUrl = "https://api.github.com/users/" + answers.github + "/repos?per_page=100";
+        const gitdata = await axios.get(reposUrl)//.then(function(reply){
+            // console.log(reply[0].name);
+            //console.log(gitdata.data[0].name);//gets name of first repo
+            //console.log(gitdata.data.length);//number of repos
+            
+            
+            //const repoNames = reply.data//.repos;
+            // });
+        }
+        catch(err){
+            console.log(err);
+            
+        };
+        // (console.log(answers.name));getting answers from prompt works
+};
+    
 function writeToFile(fileName, data) {
  
 }
